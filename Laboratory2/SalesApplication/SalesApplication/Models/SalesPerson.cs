03ca58b0e2a3d0e1c85cdbd403af12f5c4550de7
Yura <Odessa202@gmail.com>
2024-09-14 22:19:38 +0300

    Add SalesPerson class

---
+++ Laboratory2/SalesApplication/SalesApplication/Models/SalesPerson.cs
@@ -0,0 +1,36 @@
+﻿namespace SalesApplication.Models
+{
+    public class SalesPerson
+    {
+        public static decimal TargetSales { get; set; } = 200_000m;
+
+        public string Name { get; set; }
+        public decimal TotalSales { get; private set; }
+        private int NumberOfSales { get; set; }
+
+        public SalesPerson(string name)
+        {
+            Name = name;
+        }
+
+        public decimal AvgSale
+        {
+            get
+            {
+                if (NumberOfSales == 0)
+                {
+                    return 0m;
+                }
+
+                return TotalSales / NumberOfSales;
+            }
+        }
+
+        public void AddSale(decimal sale)
+        {
+            TotalSales += sale;
+            NumberOfSales++;
+        }
+
+    }
+}
